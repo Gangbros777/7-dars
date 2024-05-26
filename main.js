1c2e2b3d662bbc7273483fcabfe2614ed1eb3249
Gangbros777 <161280210+Gangbros777@users.noreply.github.com>
2024-05-26 05:53:04 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let mevalar = ["olma" , "banan" , "qulipnay" , "Shaftoli"]
+console.log(mevalar);
+alert ("Arrayni uzubligi:4")
+alert ("Sizning arrayingizdan malumotlarni olib tashamoqchimiz")
+alert ("Arrayni uzunligi:3")
+mevalar.pop(1,2,3)
+console.log(mevalar);
